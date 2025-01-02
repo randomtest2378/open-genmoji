49c98db61680f16af7e9b6ebdad0338293cec277
Evan Zhou <evanzhoudev@gmail.com>
2025-01-02 13:26:28 -0800

    ✨ New download script with LoRA selection

--- download.py
+++ download.py
@@ -1,9 +1,30 @@
+import json
 from huggingface_hub import hf_hub_download
 
-REPO_ID = "EvanZhouDev/open-genmoji"
-FILENAME = "flux-dev.safetensors"
+# Read info.json
+with open("./lora/info.json", "r") as f:
+    models = json.load(f)
 
-print(
-    "Downloaded weights to: "
-    + hf_hub_download(repo_id=REPO_ID, filename=FILENAME, local_dir="./lora")
-)
+# Print menu
+print("Available models:")
+for i, model in enumerate(models):
+    print(
+        f"[{i}] {model['name']} ({model['model'].split("/")[1]})\n> {model['description']}"
+    )
+
+# Get user choice
+choice = int(input("\nSelect a model (enter number): "))
+
+if 0 <= choice < len(models):
+    selected = models[choice]
+    filename = f"{selected['name']}.safetensors"
+
+    print(f"\nDownloading {selected['name']}...")
+    print(
+        "Downloaded weights to: "
+        + hf_hub_download(
+            repo_id=selected["huggingface"], filename=filename, local_dir="./lora"
+        )
+    )
+else:
+    print("Invalid selection")
